c694f758f9c9894c3efe674c7f2ee86cc1b6f564
Ubuntu <ubuntu@ip-172-31-94-130.ec2.internal>
2026-02-08 04:09:34 +0000

    Standardize Mongo connection to use MONGODB_URI

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     
     const dbName = "e-commerce";
-    await mongoose.connect(`${process.env.MONGO_URI}/${dbName}`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/${dbName}`);
 
     console.log(" MongoDB connected successfully to database ");
   } catch (err) {
